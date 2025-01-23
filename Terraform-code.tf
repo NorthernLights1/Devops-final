85d4b2163a69b0b7c88afc414060417a84d0e6ef
NorthernLights1 <temeg242@gmail.com>
2025-01-23 22:34:43 +0300

    Initial commit: Terraform setup for infrastructure

---
+++ Terraform-code.tf
@@ -0,0 +1,172 @@
+# Terraform code to provision 5 EC2 instances and set up integration with Ansible
+# Each instance is assigned a specific role (e.g., Jenkins Master, Kubernetes Agent, etc.)
+# Instances will be t2.micro, using a specified AMI ID, and prepared for Ansible automation
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file used to SSH into instances"
+  default     = "~/.ssh/my-key.pem"
+}
+
+variable "instance_ami" {
+  description = "AMI ID to use for all instances"
+  default     = "ami-08970251d20e940b0"
+}
+
+variable "instance_type" {
+  description = "Instance type to use for all instances"
+  default     = "t2.micro"
+}
+
+# Security group allowing SSH and HTTP/HTTPS access
+resource "aws_security_group" "common_sg" {
+  name        = "common-sg"
+  description = "Allow SSH, HTTP, and HTTPS access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Function to create an instance
+resource "aws_instance" "jenkins_master" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "my-key"
+  security_groups = [aws_security_group.common_sg.name]
+
+  tags = {
+    Name = "jenkins-master"
+  }
+
+  connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file(var.private_key_path)
+  host        = self.public_ip  # Use the instance's public IP
+}
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} jenkins-master' >> ./ansible_inventory"
+  }
+}
+
+resource "aws_instance" "jenkins_worker" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "my-key"
+  security_groups = [aws_security_group.common_sg.name]
+
+  tags = {
+    Name = "jenkins-worker"
+  }
+
+  connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file(var.private_key_path)
+  host        = self.public_ip  # Use the instance's public IP
+}
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} jenkins-worker' >> ./ansible_inventory"
+  }
+}
+
+resource "aws_instance" "kubernetes_master" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "my-key"
+  security_groups = [aws_security_group.common_sg.name]
+
+  tags = {
+    Name = "kubernetes-master"
+  }
+
+  connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file(var.private_key_path)
+  host        = self.public_ip  # Use the instance's public IP
+}
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} kubernetes-master' >> ./ansible_inventory"
+  }
+}
+
+resource "aws_instance" "kubernetes_agent" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "my-key"
+  security_groups = [aws_security_group.common_sg.name]
+
+  tags = {
+    Name = "kubernetes-agent"
+  }
+
+  connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file(var.private_key_path)
+  host        = self.public_ip  # Use the instance's public IP
+}
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} kubernetes-agent' >> ./ansible_inventory"
+  }
+}
+
+resource "aws_instance" "pg_instance" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "my-key"
+  security_groups = [aws_security_group.common_sg.name]
+
+  tags = {
+    Name = "PG-instance"
+  }
+
+  connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file(var.private_key_path)
+  host        = self.public_ip  # Use the instance's public IP
+}
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} pg-instance' >> ./ansible_inventory"
+  }
+}
+
+# Output the generated Ansible inventory
+output "ansible_inventory" {
+  value = "cat ./ansible_inventory"
+}
