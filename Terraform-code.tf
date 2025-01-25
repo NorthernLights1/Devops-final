e9a4a8ecd0b6b656306bcea754a0f47d0a48a481
NorthernLights1 <temeg242@gmail.com>
2025-01-25 23:46:17 +0300

    Added exception for 6443 for use in kubernetes

--- Terraform-code.tf
+++ Terraform-code.tf
@@ -49,6 +49,14 @@ resource "aws_security_group" "common_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+      ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
